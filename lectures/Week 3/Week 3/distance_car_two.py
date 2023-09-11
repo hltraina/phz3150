6f18dcd024d3cdae9249dcaafd14760ab27934c2
Hailey <hltraina@knights.ucf.edu>
2023-09-11 12:38:21 -0500

    Adding Homework 3
    Adding Homework 3 files to the GitHub!

---
+++ lectures/Week 3/Week 3/distance_car_two.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+def distance_car_two(u,a,t): 
+    """Here is my informative docstring that tells me what the function actually does!"""
+    s = u * t + 0.5 *a * t**2
+    return s
